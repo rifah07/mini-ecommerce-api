cc4c300b22bb3ff0c32bfc0c48cf804d4dcc7956
Rifah Sajida Deya <rifahsajida7@gmail.com>
2026-02-11 12:56:07 +0600

    feat: create product controller

---
+++ src/modules/product/controllers/product.controller.ts
@@ -0,0 +1,42 @@
+import { Response } from 'express';
+import productService from '../services/product.service';
+import { AuthRequest } from '../../../types';
+import asyncHandler from '../../../utils/asyncHandler';
+import ApiResponse from '../../../utils/apiResponse';
+import { CreateProductInput, UpdateProductInput } from '../validators/product.validator';
+
+export class ProductController {
+  createProduct = asyncHandler(async (req: AuthRequest, res: Response) => {
+    const data: CreateProductInput = req.body;
+    const product = await productService.createProduct(data);
+    return ApiResponse.created(res, { product }, 'Product created successfully');
+  });
+
+  getAllProducts = asyncHandler(async (req: AuthRequest, res: Response) => {
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+
+    const search = req.query.search ? String(req.query.search) : undefined;
+
+    const result = await productService.getAllProducts(page, limit, search);
+    return ApiResponse.success(res, result);
+  });
+
+  getProductById = asyncHandler(async (req: AuthRequest, res: Response) => {
+    const product = await productService.getProductById(req.params.id);
+    return ApiResponse.success(res, { product });
+  });
+
+  updateProduct = asyncHandler(async (req: AuthRequest, res: Response) => {
+    const data: UpdateProductInput = req.body;
+    const product = await productService.updateProduct(req.params.id, data);
+    return ApiResponse.success(res, { product }, 'Product updated successfully');
+  });
+
+  deleteProduct = asyncHandler(async (req: AuthRequest, res: Response) => {
+    const result = await productService.deleteProduct(req.params.id);
+    return ApiResponse.success(res, result);
+  });
+}
+
+export default new ProductController();
